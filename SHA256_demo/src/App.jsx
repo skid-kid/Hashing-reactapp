da85478ccc950453ce6cd4145d2d06667098b035
skid-kid <polarb587@gmail.com>
2024-08-19 17:31:41 +0530

    hehhe

--- SHA256_demo/src/App.jsx
+++ SHA256_demo/src/App.jsx
@@ -12,19 +12,19 @@ function App() {
 
   return (
     <div>
-      <h1 className='font-bold text-5xl m-3 p-3'>SHA256 HASHING</h1>
+      <h1 className='font-bold text-7xl m-3 p-3'>SHA256 HASHING</h1>
       <div className='flex m-5 p-5  bg-gray-200 box-content'>
-        <h3 className='text-3xl'>Enter Data:-</h3>
-        <input className='bg-slate-400 box-border flex-grow p-2 block' type="text" value={input} onChange={handleChange}></input>
+        <h3 className='text-5xl'>Enter Data:-</h3>
+        <input className='bg-slate-400 text-3xl box-border flex-grow p-2 block' type="text" value={input} onChange={handleChange}></input>
       </div>
       <div className='flex m-5 p-5  bg-gray-200 box-content'>
-      <h3 className='text-3xl'>Hash:-</h3>
+      <h3 className='text-5xl'>Hash:-</h3>
       <GenerateHash></GenerateHash>
       </div>
       </div>
   )
   function GenerateHash(){
-      return <div className='bg-slate-400 flex-grow p-2'>
+      return <div className='bg-slate-400 text-3xl flex-grow p-2'>
          <h3>
           {ans}
          </h3>
